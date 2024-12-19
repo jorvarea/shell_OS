283d00a05330e9079c837982da5e73806c81be15
jorvarea <jorvarea@users.noreply.github.com>
2024-12-19 15:58:58 +0100

    fix fg messages

--- src/fg.c
+++ src/fg.c
@@ -21,20 +21,18 @@ void bring_job_foreground(t_shell *shell, job *background_job)
 void fg(t_shell *shell, char **args)
 {
     shell->exit_status = 0;
-	if (found_flags(args))
-		invalid_flag_error(shell, "cd", args[1][1], "fg [job_spec]");
-	else if (count_words(args) == 1)
+	if (count_words(args) == 1)
     {
         if (shell->job_l->next)
             bring_job_foreground(shell, shell->job_l->next);
         else
-            shell_error(shell, "fg: current: no such job", 1);
+            shell_error(shell, "Job not found", 1);
     }
     else if (count_words(args) == 2)
     {
         job *background_job = get_item_bypos(shell->job_l, atoi(args[1]));
         if (background_job == NULL)
-            shell_error(shell, "fg: no such job", 1);
+            shell_error(shell, "Job not found", 1);
         else
             bring_job_foreground(shell, background_job);        
     }
