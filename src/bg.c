3705f897c13af6a39d20410c9c7d90a21fe2f653
jorvarea <jorvarea@users.noreply.github.com>
2024-12-19 18:51:04 +0100

    jobs handling better

--- src/bg.c
+++ src/bg.c
@@ -12,8 +12,10 @@ void bg(char **args)
     shell.exit_status = 0;
 	if (count_words(args) == 1)
     {
-        if (shell.job_l->next)
+        if (shell.job_l->next && shell.job_l->next->state == STOPPED)
             continue_job_background(shell.job_l->next);
+        else if(shell.job_l->next && shell.job_l->next->state == BACKGROUND)
+            shell_error("Job already in background", 0);
         else
             shell_error("Job not found", 1);
     }
@@ -22,7 +24,9 @@ void bg(char **args)
         job *background_job = get_item_bypos(shell.job_l, atoi(args[1]));
         if (background_job == NULL)
             shell_error("Job not found", 1);
-        else
+        else if(background_job && background_job->state == BACKGROUND)
+            shell_error("Job already in background", 0);
+        else if (background_job->state == STOPPED)
             continue_job_background(background_job);
     }
     else
